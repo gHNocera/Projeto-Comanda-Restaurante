df0f32bd2500a6d18a975ba05753acdf1003dd63
gHartmann <hartmanngabrielno@outlook.com>
2025-03-08 12:16:43 -0300

    First Commit
    Cria um prato

---
+++ comandaproject/src/main/java/com/ghartmann/Prato.java
@@ -0,0 +1,17 @@
+package com.ghartmann;
+
+
+class Prato {
+
+    String foto;
+
+    String nomePrato;
+
+    String descricao;
+
+    String observacao;
+
+    static void add(String foto, String nomePrato, String descricao) {
+    }
+
+}
