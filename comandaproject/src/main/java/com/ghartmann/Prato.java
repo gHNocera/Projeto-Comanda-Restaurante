2b951583db8f846a4cec4bdb71d12ed6c4be3fef
gHartmann <hartmanngabrielno@outlook.com>
2025-03-14 19:09:55 -0300

    Comentarios e Hibernate
    Tentando configurar o hibernate

--- comandaproject/src/main/java/com/ghartmann/Prato.java
+++ comandaproject/src/main/java/com/ghartmann/Prato.java
@@ -1,18 +1,43 @@
 package com.ghartmann;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="PRATO_TABLE")
 class Prato {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="id_prato_sequence")
+    @SequenceGenerator(name="id_prato_sequence", sequenceName="prato_seq")
     int id;
 
+    @Column(name="fotoPrato")
     String foto;
 
+    @Column(name="nomePrato")
     String nomePrato;
 
+    @Column(name="descPrato")
     String descricao;
 
+    @Column(name="obs")
     String observacao;
 
+    @Column(name="numeroMesa")
     int numeroMesa;
+
+    //*Um prato para muitas mesas */
+    @ManyToOne
+    @JoinColumn(name="mesa_id")
+    Mesa mesa;
 
     public String getFoto() {
         return foto;
